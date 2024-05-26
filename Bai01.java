ae1b150a5e33bd07360e1212587d60ea96a21dce
DESKTOP-5CLUJLF\hongthai <hongthaiptit303@gmail.com>
2024-05-26 16:31:21 +0700

    ttew

---
+++ Bai01.java
@@ -0,0 +1,90 @@
+package lyThuyet;
+
+import common.BaseTest;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Bai01  extends BaseTest {
+//    public WebDriver driver;
+//
+//    @BeforeMethod
+//    public WebDriver createDriver() {
+//        System.setProperty("webdriver.chrome.driver", "src/test/java/webDriver/chromedriver.exe");
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        return driver;
+//    }
+//
+//    @AfterMethod
+//    public void closeDriver() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//    }
+
+    @Test
+    public void test01() {
+        driver.get("https://automationfc.github.io/basic-form/index.html");
+        sleepInSecond(3);
+        //check isDisplay mail:
+        if (driver.findElement(By.id("mail")).isDisplayed()) {
+            System.out.println("element is displayed");
+        } else {
+            System.out.println("element isnt displayed");
+        }
+        Assert.assertEquals(curentUrl, "http://live.techpanda.org/index.php/customer/account/login/");
+        //Click on create account:
+        driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+        sleepInSecond(2);
+        Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
+    }
+
+    @Test
+    public void test02() {
+        driver.get("https://automationfc.github.io/basic-form/index.html");
+        sleepInSecond(3);
+        //check isDisplay mail:
+        if (driver.findElement(By.id("mail")).isDisplayed()) {
+            System.out.println("element is displayed");
+        } else {
+            System.out.println("element isnt displayed");
+        }
+
+        Assert.assertTrue(driver.findElement(By.id("mail")).isDisplayed());
+
+        //Check isEnable email:
+        if (driver.findElement(By.id("mail")).isEnabled()) {
+            System.out.println("element is isEnabled");
+        } else {
+            System.out.println("element isnt isEnabled");
+        }
+        Assert.assertTrue(driver.findElement(By.id("mail")).isEnabled());
+        //Check disable:
+        if (driver.findElement(By.id("disable_password")).isEnabled()) {
+            System.out.println("element is isEnabled");
+        } else {
+            System.out.println("element is disable");
+        }
+        Assert.assertFalse(driver.findElement(By.id("disable_password")).isEnabled());
+        //is selected:
+        driver.findElement(By.id(("under_18"))).click();
+        if (driver.findElement(By.id("under_18")).isSelected()) {
+            System.out.println("element is isSelected");
+        } else {
+            System.out.println("element isnt isSelected");
+        }
+        Assert.assertTrue(driver.findElement(By.id("under_18")).isSelected());
+    }
+
+    public void sleepInSecond(long timeInSecond) {
+        try {
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Sleep in " + timeInSecond + " seconds");
+    }
+
+}
+
